5f4807b202aee4c396818ccda53b915ee6dbee46
JustPil <88640664+JustPil@users.noreply.github.com>
2022-02-15 08:00:51 -0800

    Minor style updates

--- src/ba/HexArithmetic.java
+++ src/ba/HexArithmetic.java
@@ -3,8 +3,7 @@ package ba;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HexArithmetic
-{
+public class HexArithmetic {
     private final int HEXADECIMAL_BASE = 16;
     private HashMap<Character, Integer> mapGetInt = new HashMap<>();
     private HashMap<Integer, Character> mapGetChar = new HashMap<>();
@@ -12,8 +11,7 @@ public class HexArithmetic
     /**
      * Constructor calls the populateMap method to populate the class's Map variables.
      */
-    public HexArithmetic()
-    {
+    public HexArithmetic() {
         populateMap();
     }
 
@@ -22,11 +20,9 @@ public class HexArithmetic
      * @param s The string representing a hexadecimal number.
      * @return The decimal conversion of the hexadecimal number.
      */
-    public int hexToDecimal(String s)
-    {
+    public int hexToDecimal(String s) {
         int result = 0, counter = 0;
-        for(int i = s.length() - 1; i >= 0; i--)
-        {
+        for(int i = s.length() - 1; i >= 0; i--) {
             result += mapGetInt.get(s.charAt(i)) * Math.pow(HEXADECIMAL_BASE, counter++);
         }
         return result;
@@ -37,13 +33,11 @@ public class HexArithmetic
      * @param dec The decimal number to convert.
      * @return A string containing the decimal number's hexadecimal conversion.
      */
-    public String decimalToHex(int dec)
-    {
+    public String decimalToHex(int dec) {
         double d = Math.log(dec) / Math.log(HEXADECIMAL_BASE);
         final int SIZE = (int)d + 1;
         StringBuilder sb = new StringBuilder(SIZE);
-        for(int i = 0; i < SIZE; i++)
-        {
+        for(int i = 0; i < SIZE; i++) {
             sb.append(mapGetChar.get(dec % HEXADECIMAL_BASE));
         }
         return sb.reverse().toString();
@@ -56,21 +50,17 @@ public class HexArithmetic
      * @param s2 The second string representing a hexadecimal number.
      * @return A string containing the sum of the first and second hexadecimal numbers.
      */
-    public String addHexStrings(String s1, String s2)
-    {
-        if(s1.length() > s2.length())
-        {
+    public String addHexStrings(String s1, String s2) {
+        if(s1.length() > s2.length()) {
             s2 = padString(s2, s1.length());
         }
-        if(s1.length() < s2.length())
-        {
+        if(s1.length() < s2.length()) {
             s1 = padString(s1, s2.length());
         }
         int[] result = new int[s1.length()];
         int carry = 0;
         int digit;
-        for(int i = s1.length() - 1; i >= 0; i--)
-        {
+        for(int i = s1.length() - 1; i >= 0; i--) {
             int first = mapGetInt.get(s1.charAt(i));
             int second = mapGetInt.get(s2.charAt(i));
             digit = first + second + carry;
@@ -78,12 +68,10 @@ public class HexArithmetic
             result[i] = digit % HEXADECIMAL_BASE;
         }
         StringBuilder sb = new StringBuilder(result.length);
-        for(int val : result)
-        {
+        for(int val : result) {
             sb.append(mapGetChar.get(val));
         }
-        if(carry > 0)
-        {
+        if(carry > 0) {
             sb.reverse();
             sb.append(mapGetChar.get(carry));
         }
@@ -97,19 +85,16 @@ public class HexArithmetic
      * @param s A string representing a hexadecimal number of any size up to 1000.
      * @return A string representing a hexadecimal product of the two input parameters.
      */
-    public String multiplyHex(char c, String s)
-    {
+    public String multiplyHex(char c, String s) {
         int carry = 0;
         StringBuilder product = new StringBuilder();
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i = s.length() - 1; i >= 0; i--)
-        {
+        for(int i = s.length() - 1; i >= 0; i--) {
             list.add((mapGetInt.get(s.charAt(i)) * mapGetInt.get(c) + carry) % HEXADECIMAL_BASE);
             carry = (mapGetInt.get(s.charAt(i)) * mapGetInt.get(c) + carry) / HEXADECIMAL_BASE;
         }
         list.add(carry);
-        for (Integer integer : list)
-        {
+        for(Integer integer : list) {
             product.append(mapGetChar.get(integer));
         }
         return product.reverse().toString();
@@ -121,16 +106,14 @@ public class HexArithmetic
      * @param len The length that the string needs to reach.
      * @return The string with padded zero characters.
      */
-    private String padString(String s, int len)
-    {
+    private String padString(String s, int len) {
         return String.format("%1$" + len + "s", s).replace(' ', '0');
     }
 
     /**
      * populateMap Populates the class's Map variables with hexadecimal characters and their decimal equivalents.
      */
-    private void populateMap()
-    {
+    private void populateMap() {
         mapGetInt.put('0', 0); mapGetInt.put('1', 1); mapGetInt.put('2', 2); mapGetInt.put('3', 3);
         mapGetInt.put('4', 4); mapGetInt.put('5', 5); mapGetInt.put('6', 6); mapGetInt.put('7', 7);
         mapGetInt.put('8', 8); mapGetInt.put('9', 9); mapGetInt.put('A', 10); mapGetInt.put('B', 11);
